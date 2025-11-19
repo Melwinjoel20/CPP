e190178a369ae22e32e248c4d483a05fb25d86ab
Melwinjoel20 <x24265438@student.ncirl.ie>
2025-11-19 22:03:11 +0000

    Delete Pract 2 directory

--- Pract 2/Main.py
+++
@@ -1,8 +0,0 @@
-class user_details():
-    def __init__(self, age, height):
-        self.age = age
-        self.height = height
-    
-    def weight(self):
-        RW = (self.height - 100 + self.age % 10) * 0.90
-        return "Your weight is:{}".format(RW)
--- Pract 2/input.py
+++
@@ -1,7 +0,0 @@
-from Main import user_details
-a_age = int(input ("Enter the age:"))
-a_height = int(input("Enter the height:"))
-
-Weight = user_details(a_age, a_height)
-obj = Weight.weight()
-print(obj)
